4763627d1baa65e23fb4c6448d67b0e057291b98
codewmanas <manas.kolaskar@somaiya.edu>
2025-02-16 14:26:44 +0530

    TextField Learned

--- currency_convertor/lib/currency_convertor_material_page.dart
+++ currency_convertor/lib/currency_convertor_material_page.dart
@@ -5,9 +5,34 @@ class CurrencyConvertorMaterialPage extends StatelessWidget {
   const CurrencyConvertorMaterialPage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
+      backgroundColor: Colors.black,
       body: Center(
-        child: Text('Hello World'),
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text('0',
+          style: TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+            color: Colors.white
+          ),
+          ),
+          TextField(
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+            decoration: InputDecoration(
+              label: Text('Enter Amount in INR', style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),),
+            ),
+          ),
+        ],
+      ),
       ),
     );
 
