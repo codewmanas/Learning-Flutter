f5e827b8ea3b73e7be0bd01057e0282f7b508a94
codewmanas <manas.kolaskar@somaiya.edu>
2025-02-28 22:46:15 +0530

    design

--- currency_convertor/lib/currency_convertor_material_page.dart
+++ currency_convertor/lib/currency_convertor_material_page.dart
@@ -5,7 +5,7 @@ class CurrencyConvertorMaterialPage extends StatelessWidget {
   const CurrencyConvertorMaterialPage({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.black,
       body: Center(
         child: Column(
@@ -19,25 +19,51 @@ class CurrencyConvertorMaterialPage extends StatelessWidget {
             color: Colors.white
           ),
           ),
-          TextField(
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 20,
-            ),
-            decoration: InputDecoration(
-              hintText: 'Enter Amount in INR',
-              hintStyle: TextStyle(
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: TextField(
+              style: TextStyle(
                 color: Colors.black,
                 fontSize: 20,
               ),
-              prefixIcon: Icon(
-                Icons.monetization_on,
-                color: Colors.black,
+              decoration: InputDecoration(
+                hintText: 'Enter Amount in INR',
+                hintStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                ),
+                prefixIcon: Icon(
+                  Icons.monetization_on,
+                  color: Colors.black,
+                ),
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(50)),
+                ),
               ),
-              filled: true,
-              fillColor: Colors.white,
+              keyboardType:  TextInputType.numberWithOptions(
+                decimal: true,
+                signed: false,
+              ),
             ),
           ),
+          TextButton(onPressed: () {
+            print('Button Clicked');
+          }, 
+          style: ButtonStyle(
+            backgroundColor: WidgetStateProperty.all(Colors.white),
+            textStyle: WidgetStateProperty.all(
+              TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+              ),
+              
+            ),
+          ),
+          child: const Text('Convert'),
+          ),
+          
         ],
       ),
       ),
