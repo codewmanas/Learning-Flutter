f78b75345b16d646a66951c215cc1190c8d39558
codewmanas <manas.kolaskar@somaiya.edu>
2025-03-01 11:14:41 +0530

    Material Currency convertor

--- currency_convertor/lib/currency_convertor_material_page.dart
+++ currency_convertor/lib/currency_convertor_material_page.dart
@@ -1,73 +1,75 @@
 import 'package:flutter/material.dart';
 
-class CurrencyConvertorMaterialPage extends StatelessWidget {
-
+class CurrencyConvertorMaterialPage extends StatefulWidget {
   const CurrencyConvertorMaterialPage({super.key});
+
   @override
+  State<CurrencyConvertorMaterialPage> createState() => _CurrencyConvertorMaterialPageState();
+}
+
+class _CurrencyConvertorMaterialPageState extends State<CurrencyConvertorMaterialPage> {
+  final TextEditingController textEditingController = TextEditingController();
+  double result = 0;
+  @override
+  void initState() {
+
+    super.initState();
+  }
   Widget build(BuildContext context) {
+    
     return Scaffold(
       backgroundColor: Colors.black,
       body: Center(
         child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text('0',
-          style: TextStyle(
-            fontSize: 40,
-            fontWeight: FontWeight.bold,
-            color: Colors.white
-          ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: TextField(
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 20,
-              ),
-              decoration: InputDecoration(
-                hintText: 'Enter Amount in INR',
-                hintStyle: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                ),
-                prefixIcon: Icon(
-                  Icons.monetization_on,
-                  color: Colors.black,
-                ),
-                filled: true,
-                fillColor: Colors.white,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(50)),
-                ),
-              ),
-              keyboardType:  TextInputType.numberWithOptions(
-                decimal: true,
-                signed: false,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'INR ${result}',
+              style: const TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
               ),
             ),
-          ),
-          TextButton(onPressed: () {
-            print('Button Clicked');
-          }, 
-          style: ButtonStyle(
-            backgroundColor: WidgetStateProperty.all(Colors.white),
-            textStyle: WidgetStateProperty.all(
-              TextStyle(
-                color: Colors.black,
-                fontSize: 20,
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextField(
+                controller: textEditingController,
+                style: TextStyle(color: Colors.black, fontSize: 20),
+                decoration: InputDecoration(
+                  hintText: 'Enter Amount in INR',
+                  hintStyle: TextStyle(color: Colors.black, fontSize: 20),
+                  prefixIcon: Icon(Icons.monetization_on, color: Colors.black),
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(50)),
+                  ),
+                ),
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: true,
+                  signed: false,
+                ),
               ),
-              
             ),
-          ),
-          child: const Text('Convert'),
-          ),
-          
-        ],
-      ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  result = double.parse(textEditingController.text) * 87 ;
+                });
+              },
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all(Colors.white),
+                textStyle: WidgetStateProperty.all(
+                  TextStyle(color: Colors.black, fontSize: 20),
+                ),
+              ),
+              child: const Text('Convert'),
+            ),
+          ],
+        ),
       ),
     );
-
   }
 }
