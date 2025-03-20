dd932f80315429d4a045a16f458d73cde52a07e4
codewmanas <manas.kolaskar@somaiya.edu>
2025-03-20 11:29:51 +0530

    main card Designed

--- weather_app/lib/weather_screen.dart
+++ weather_app/lib/weather_screen.dart
@@ -7,7 +7,63 @@ class WeatherScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(
-        title: const Text('Weather App'),
+        title: const Text(
+          'Weather App', 
+          style: TextStyle(
+          fontWeight: FontWeight.bold
+          ),
+          ),
+          centerTitle: true,
+          actions:  [
+            IconButton(onPressed: (){
+              
+            }, 
+            icon: const Icon(Icons.refresh),
+            ),
+          ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            //main card 
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(children: [
+                    Text('300°K',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    ),
+                    const SizedBox(height: 16),
+                    Icon(Icons.cloud, size: 64,),
+                    const SizedBox(height: 16),
+                    Text('Rain',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                    )
+                  ],
+                  ),
+                ),
+              ),
+            ),
+            //weather forecast cards
+            const SizedBox(height: 20),
+            const Placeholder(
+              fallbackHeight: 150,
+            ),
+            const SizedBox(height: 20),
+            //additional information
+            const Placeholder(
+              fallbackHeight: 150,
+            ),
+          ],
+        ),
       ),
     );
   }
